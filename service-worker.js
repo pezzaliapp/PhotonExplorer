e85261d05175d5f3cd707ea841c4a598cd7ca255
pezzaliapp <pezzalialessandro@gmail.com>
2025-01-26 13:02:22 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,26 +1,28 @@
 const cacheName = "photonExplorerCache-v1";
 const assets = [
-    "/",
-    "/index.html",
-    "/style.css",
-    "/app.js",
-    "/manifest.json",
-    "/icons/icon-192x192.png",
-    "/icons/icon-512x512.png"
+  "/",
+  "/index.html",
+  "/style.css",
+  "/app.js",
+  "/manifest.json",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png",
+  "https://unpkg.com/leaflet@1.9.3/dist/leaflet.css",
+  "https://unpkg.com/leaflet@1.9.3/dist/leaflet.js"
 ];
 
 self.addEventListener("install", (event) => {
-    event.waitUntil(
-        caches.open(cacheName).then((cache) => {
-            return cache.addAll(assets);
-        })
-    );
+  event.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      return cache.addAll(assets);
+    })
+  );
 });
 
 self.addEventListener("fetch", (event) => {
-    event.respondWith(
-        caches.match(event.request).then((response) => {
-            return response || fetch(event.request);
-        })
-    );
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
